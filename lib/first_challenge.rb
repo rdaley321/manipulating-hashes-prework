3644dddb7f8a4c6b9c3021fd1c180a67ad4daee7
Ryan Daley <rdaley321@yahoo.com>
2018-11-07 10:01:21 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ contacts.each do |name,data|
   if name == "Freddy Mercury"
     data.each do |more_data, more_value|
       if more_data == :favorite_icecream_flavors
-        more_value.delete_if(|value| value == "strawberry")
+        more_value.delete_if(|flavors| flavors == "strawberry")
       end
     end
   end
@@ -26,5 +26,3 @@ end
   #remember to return your newly altered contacts hash!
   contacts
 end
-
-first_challenge 
