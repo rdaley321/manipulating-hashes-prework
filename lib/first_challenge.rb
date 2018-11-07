149356ba8b4e4c5cd3a09cff7f00a2479f7e4f04
Ryan Daley <rdaley321@yahoo.com>
2018-11-07 09:50:48 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,16 +12,14 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-i=-1
   #your code here
 contacts.each do |name,data|
   if name == "Freddy Mercury"
     data.each do |more_data, more_value|
       if more_data == "favorite_icecream_flavors"
         more_value.each do |arr_value|
-          i+=1
           if arr_value == "strawberry"
-            more_data.delete_at(i)
+            more_data.delete("strawberry")
           end
         end
       end
@@ -33,3 +31,4 @@ end
   contacts
 end
 
+first_challenge 
