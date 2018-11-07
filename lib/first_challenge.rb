b806d0a048d9b63227188c19f38909ebeff90acc
Ryan Daley <rdaley321@yahoo.com>
2018-11-07 09:39:33 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,18 @@ def first_challenge
   }
 
   #your code here
-
+contacts.each do |key,value|
+  if key == "Freddy Mercury"
+    value.each do |key, value|
+      if key == "favorite_icecream_flavors"
+        value.each do |arr_value|
+          if arr_value == "strawberry"
+            
+          end
+        end
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
